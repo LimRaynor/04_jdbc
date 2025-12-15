c532b8b4ca566aebfcbb36cd518b623c1402ed1e
choihyeonji00 <chlguswl05@naver.com>
2025-12-16 08:50:10 +0900

    1215과제

--- 16_ChoiHyeonJi/chap01-connection-lecture-source/src/main/java/com/ohgiraffers/section01/connection/Application2.java
+++ 16_ChoiHyeonJi/chap01-connection-lecture-source/src/main/java/com/ohgiraffers/section01/connection/Application2.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.section02.template;
+package com.ohgiraffers.section01.connection;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -7,34 +7,39 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-/* 반복되는 JDBC 관련 코드를 미리 작성해서 Util용으로 사용할
-  JDBCTemplate 클래스를 만들 수 있다.*/
-public class JDBCTemplate {
+/* properties 파일 내용을 읽어와 DB와 연결할 수 있다 */
+public class Application2 {
 
-  public static Connection getConnection(){
+  public static void main(String[] args) {
 
     Properties prop = new Properties();
     Connection con = null;
 
     try{
       prop.load(new FileReader("src/main/java/com/ohgiraffers/section01/connection/jdbc-config.properties"));
-
+  
       // properties 파일에서 읽어온 데이터를 Key를 통해 얻어옴
       String url = prop.getProperty("url");
       String user = prop.getProperty("user");
       String password = prop.getProperty("password");
 
       con = DriverManager.getConnection(url, user, password);
-
+      System.out.println("con = " + con);
+      
     }catch (SQLException e){
       e.printStackTrace();
     }catch (IOException e){
       e.printStackTrace();
+    } finally {
+      try {
+        con.close();
+      }catch (SQLException e){
+        e.printStackTrace();
+      }
     }
 
-    return con;
+
+
   }
-
-
 
 }
